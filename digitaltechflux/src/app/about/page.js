c04cc4048904e09e16650c58ade3576eaf0c4606
sudip khatri <sudpkhatri4@gmail.com>
2023-08-02 20:48:44 +1000

    first commit

---
+++ digitaltechflux/src/app/about/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import { Button } from "antd";
+
+const Home = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default Home;
---
+++ digitaltechflux/src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+
+  <div className="App">
+    <h1>Home</h1>
+  </div>
+
+  )
+}
+
