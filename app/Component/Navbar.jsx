0952724f4f7a9bf4e373567174a0690fc146dfea
samin99kc <saminkc99@gmail.com>
2023-08-05 09:52:56 +0545

    feat: menu create homepage

--- app/Component/Navbar.jsx
+++ app/Component/Navbar.jsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 import NavItem from "./NavItem";
 
 const Menu_List = [
-  { text: "about us", href: "/services" },
+  { text: "About us", href: "/about" },
   { text: "service", href: "/privacypolicy" },
   { text: "Blog", href: "/privacypolicy" },
   { text: "Terms & Conditions", href: "/terms-and-conditions" },
@@ -14,24 +14,33 @@ const Navbar = () => {
   const [navActive, setNavActive] = useState(null);
   const [activeIdx, setNavActiveIdx] = useState(-1);
   return (
-    <div className="sticky z-30 top-0 bg-white">
-      <nav className={`nav ${navActive ? "active" : ""}`}>
-        <Link href={"/"}>
-          <span onClick={() => setNavActiveIdx(-1)}>
-            <h1 className="text-xl font-semibold">DigitalFlux</h1>
-          </span>
-        </Link>
-        <div
-          className={`menu_icon ${navActive ? "active" : ""}`}
-          onClick={() => setNavActive(!navActive)}
-        >
-          <div></div>
-          <div></div>
-          <div></div>
+    <div className="sticky z-30 top-0 shadow-xl	p-6 bg-[#100546]">
+      <nav className={`nav flex justify-center${navActive ? "active" : ""}`}>
+        <div className="basis-1/4">
+          <Link href={"/"}>
+            <span onClick={() => setNavActiveIdx(-1)}>
+              <h1 className="text-xl font-semibold text-blue-600">
+                DigitalFlux
+              </h1>
+            </span>
+          </Link>
+          <div
+            className={`menu_icon ${navActive ? "active" : ""}`}
+            onClick={() => setNavActive(!navActive)}
+          >
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
-        <div className={`nav_menu ${navActive ? "active" : ""}`}>
+        <div
+          className={`nav_menu flex basis-1/2 justify-end	 ${
+            navActive ? "active" : ""
+          }`}
+        >
           {Menu_List.map((menu, index) => (
             <div
+              className="pr-4 text-white"
               onClick={() => {
                 setNavActiveIdx(index);
                 setNavActive(false);
